482d0cd3f779157edc4da762b3c0d925a6d02304
Philipp Johag <johag@uni-bremen.de>
2019-02-28 21:01:14 +0200

    add disclaimer for time window

--- screens/OrderWhenScreen.js
+++ screens/OrderWhenScreen.js
@@ -40,20 +40,25 @@ class OrderWhenScreen extends React.Component {
 
     render() {
         const currentOrderData = displayOrderData(this.state.orderData);
+        const timeDisclaimer = this.state.orderData.leaving ? 'The car may get to you up to 25 minutes earlier or later than ordered!' : 'The car may arrive at your destination up to 25 minutes earlier than ordered!';
 
         //TODO: use more user-friendly input type
         //inputs for date and time of trip plus whether customer wants to leave or arrive at given time
         const input = <View>
-
-            {createTextInput("Enter date and time", this.setDateTime)}
-
             <View>
-                <Text>{this.state.orderData.leaving ? 'Order to leave at given time' : 'Order to arrive at given time'}</Text>
                 <Switch
                     style={{ marginTop: 30 }}
                     onValueChange={this.setLeaving}
                     value={this.state.orderData.leaving} />
+                <Text>{this.state.orderData.leaving ? 'Order to leave at given time' : 'Order to arrive at given time'}</Text>
             </View>
+
+            <View>
+                <Text>{timeDisclaimer}</Text>
+                {createTextInput("Enter date and time", this.setDateTime)}
+            </View>
+
+
         </View>;
 
         const footer = createButton(() => this.props.navigation.navigate('OrderPassengers', { orderData: this.state.orderData }), 'Confirm Time');
