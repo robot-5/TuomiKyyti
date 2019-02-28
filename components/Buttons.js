745343afceb181a04f73ac0f74fbe0624a241947
ljeun <vcp.jeongeun@gmail.com>
2019-03-01 01:20:10 +0200

    Modify style codes

--- components/Buttons.js
+++ components/Buttons.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Layout from '../constants/Layout';
 import Colors from '../constants/Colors';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as loc,
+  removeOrientationListener as rol
+} from 'react-native-responsive-screen';
 
 /**
  * Creates a Button based on TouchableOpacity Component with given action and text. Text will be in upper case.
@@ -22,13 +28,13 @@ const buttonStyles = StyleSheet.create({
     // Button that spans the whole width of the screen
     wideButton:
     {
-        height: 100,
-        width: Layout.window.width,
+        width: wp('80%'),
+        height: hp('10%'),
         alignItems: 'center',
         backgroundColor: Colors.button,
         padding: 10,
-        borderRadius: 40,
-
+        borderRadius: 30,
+        marginBottom: 40,
     },
     text:
     {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Layout from '../constants/Layout';
 import { createButton } from '../components/Buttons';
 import { createTextInput } from '../components/TextInput';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as loc,
+  removeOrientationListener as rol
+} from 'react-native-responsive-screen';
 
 class LoginScreen extends React.Component {
 
@@ -30,19 +36,37 @@ class LoginScreen extends React.Component {
 
     render() {
         const { navigate } = this.props.navigation;
+        const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      },
+      responsiveBox: {
+        flex:2,
+        padding: 10,
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center'
+      },
+    });
 
 
 
         return (
-            <View
-                style={{ height: Layout.window.height, flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+            <View style={styles.container}>
+        <View style={styles.responsiveBox}>
+
 
                 {createTextInput("Your first and last name", this.setCustomerName)}
 
                 {createTextInput("Your customer number", this.setCustomerNumber)}
+                </View>
 
                 {/* TODO: verify customer name + number against backend data, persist name + number locally */}
                 {createButton(() => navigate('Home', { customerName: this.state.customerName, customerNumber: this.state.customerNumber }), 'Login')}
+
 
             </View>
         );
